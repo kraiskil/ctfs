ff420ddebd133c7e000b99bbd68b3d960c202b1e
Kalle Raiskila <kraiskil@iki.fi>
2020-03-06 11:53:13 +0200

    Add a few FFT tests.
    These are "real world", not corner-case tests.
    Probably not good enough for production, but good enough
    for frogs.

--- tests/fft.cc
+++ tests/fft.cc
@@ -1,20 +1,33 @@
 #include "gtest/gtest.h"
+#include <algorithm>
 #include <cmath>
 
 #include "fft.h"
 
-class fft_test : public testing::Test
+
+struct bin_value
+{
+	int bin;
+	int16_t value;
+};
+
+class fftTest :
+	public testing::TestWithParam<int32_t>
 {
 public:
 	void SetUp(void) override
 	{
-		fs = 8000;
+		fs = GetParam();
 		fft_size = 256;
+		if (fs > 16000)
+			fft_size = 512;
+		if (fs > 44100)
+			fft_size = 1024;
 		in = (int16_t*)calloc(fft_size, sizeof(int16_t));
 		out = (int16_t*)calloc(fft_size, sizeof(int16_t));
 
-		/* How wide is half a bin, in Hz */
-		bin_accuracy = ((float)fs / 2) / fft_size;
+		/* Width of a bin, in Hz */
+		bin_accuracy = ((float)fs) / fft_size;
 	}
 
 	void add_sine(int peak, int f_input)
@@ -22,6 +35,92 @@ public:
 		for (int i = 0; i < fft_size; i++) {
 			in[i] += peak * sin(2 * M_PI * i * f_input / fs);
 		}
+	}
+
+	auto magnitude(auto d)
+	{
+		return std::abs(d);
+	}
+	// biggest value first
+	void sort_by_value(std::vector<bin_value>& values)
+	{
+		std::sort(values.begin(), values.end(),
+		    [] (struct bin_value& a, struct bin_value& b)
+		    {
+			    return a.value > b.value;
+		    }
+		    );
+	}
+	void sort_by_bin(std::vector<bin_value>& values)
+	{
+		std::sort(values.begin(), values.end(),
+		    [] (struct bin_value& a, struct bin_value& b)
+		    {
+			    return a.bin < b.bin;
+		    }
+		    );
+	}
+	std::vector<bin_value> max_values(int16_t *data, int n_ret)
+	{
+		int                    max_i = -1;
+		double                 max_a = -1;
+		std::vector<bin_value> values;
+		for (int i = 0; i < fft_size / 2; i++) {
+			struct bin_value b = { /*.bin=*/
+				i, /*.value =*/ data[i]
+			};
+			values.push_back(b);
+		}
+
+		sort_by_value(values);
+
+		std::vector<bin_value> rv(values.begin(), values.begin() + n_ret);
+		return rv;
+	}
+
+	/* Merge neighbouring bins into one.
+	 * I.e. if the spectrum of a tone is not sharp (timing erros, low fs, short fft size..)
+	 * it spreads over several bins.
+	 * This joins such bins into one (the one with highest value of the group) and gives
+	 * the new bin_value a value that is the sum of its neighbours */
+	std::vector<bin_value> merge_neighbours(const auto& data)
+	{
+		std::vector<bin_value> input = data;
+		std::vector<bin_value> output;
+		sort_by_bin(input);
+
+		bool      prev_consecutive = false;
+		double    merged_value = 0;
+		bin_value max_merged = { 0, 0 };
+		for (int i = 0; i < input.size() + 1; i++) {
+			int       thisbin = input[i].bin;
+			int       nextbin = input[i + 1].bin;
+			double    thisval = input[i].value;
+			bool      next_consecutive = (nextbin - thisbin == 1);
+			bin_value current_binval;
+
+			/* This is not a continuation from the previous one */
+			if (prev_consecutive == false) {
+				merged_value = 0;
+				max_merged = input[i];
+			}
+
+			/* Merge this bin with current range */
+			merged_value += thisval;
+			if (thisval > max_merged.value)
+				max_merged = input[i];
+
+
+			/* If this is the last of the range */
+			if (next_consecutive == false) {
+				current_binval.bin = max_merged.bin;
+				current_binval.value = merged_value;
+				output.push_back(current_binval);
+			}
+
+			prev_consecutive = next_consecutive;
+		}
+		return output;
 	}
 
 	int fs;
@@ -54,10 +153,10 @@ TEST(fft, indexToFrequency)
 	EXPECT_EQ(468, index_to_frequency(15, fs, fft_size) );
 }
 
-TEST_F(fft_test, one_sine)
+TEST_P(fftTest, oneSine)
 {
 	add_sine(
-		256,  //amplitude
+		8,    //amplitude
 		500); //frequency
 
 	frog_fft(in, out, fft_size);
@@ -69,24 +168,40 @@ TEST_F(fft_test, one_sine)
 		bin_accuracy);
 }
 
-TEST_F(fft_test, many_sines)
+TEST_P(fftTest, manySines)
 {
 	add_sine(
-		100,  //amplitude
-		500); //frequency
+		10,   //amplitude
+		200); //frequency
 	add_sine(
-		80,    //amplitude
-		1000); //frequency
+		20,   //amplitude
+		900); //frequency
 	add_sine(
-		50,    //amplitude
-		1200); //frequency
+		30,    //amplitude
+		1400); //frequency
 
 	frog_fft(in, out, fft_size);
 
-	int max_i = index_of_peak(out, fft_size / 2);
-	ASSERT_NEAR(
-		500,
-		index_to_frequency(max_i, fs, fft_size),
+	std::vector<bin_value> raw_bins = max_values(out, 6);
+	std::vector<bin_value> max_bins = merge_neighbours(raw_bins);
+	sort_by_value(max_bins);
+
+	/* The three sines in input should not be bin-merged */
+	ASSERT_GE(max_bins.size(), 3);
+
+	EXPECT_NEAR(
+		1400,
+		index_to_frequency(max_bins[0].bin, fs, fft_size),
+		bin_accuracy);
+	EXPECT_NEAR(
+		900,
+		index_to_frequency(max_bins[1].bin, fs, fft_size),
+		bin_accuracy);
+	EXPECT_NEAR(
+		200,
+		index_to_frequency(max_bins[2].bin, fs, fft_size),
 		bin_accuracy);
 }
+INSTANTIATE_TEST_CASE_P(Random_fs_values, fftTest, testing::Range(6000, 48000, 6100));
+INSTANTIATE_TEST_CASE_P(Probable_fs_values, fftTest, testing::Values(8000, 16000, 24000, 44100, 48000));
 
