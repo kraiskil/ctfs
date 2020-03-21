04cad892ab6b7c2bca52ed2c4b728e828243ad52
Kalle Raiskila <kraiskil@iki.fi>
2020-03-21 18:43:57 +0200

    OpenCM3 target: debug prints and F4 board.

--- target/opencm3/main.cc
+++ target/opencm3/main.cc
@@ -1,29 +1,53 @@
 #include "treefrog.h"
 
+#ifdef STM32F4
+#include "stm32f4_config.h"
+#else
+#error Need a target board config file
+#endif
+
 extern "C" {
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/usart.h>
 }
-#define PORT_LED GPIOC
-#define PIN_LED GPIO8
+
+/* TODO: move these functions to dedicated board config files */
 static void gpio_setup(void)
 {
-	/* Enable GPIOC clock. */
-	/* Manually: */
-	//RCC_AHBENR |= RCC_AHBENR_GPIOCEN;
-	/* Using API functions: */
-	rcc_periph_clock_enable(RCC_GPIOC);
+	/* PortA is used for:
+	 *  USART
+	 * PortD is used for:
+	 *  Heartbeat LED
+	 */
+	rcc_periph_clock_enable(RCC_GPIOA);
+	rcc_periph_clock_enable(RCC_GPIOD);
 
+	gpio_mode_setup(PORT_HEARTBEAT_LED, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, PIN_HEARTBEAT_LED);
+}
 
-	/* Set GPIO8 (in GPIO port C) to 'output push-pull'. */
-	/* Using API functions: */
-	gpio_mode_setup(PORT_LED, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, PIN_LED);
+static void debuggig_usart_setup(void)
+{
+	rcc_periph_clock_enable(RCC_USART2);
+
+	/* Connects PA2 to USART2 TX */
+	gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO2);
+	gpio_set_af(GPIOA, GPIO_AF7, GPIO2);
+
+	usart_set_baudrate(USART2, 115200);
+	usart_set_databits(USART2, 8);
+	usart_set_stopbits(USART2, USART_STOPBITS_1);
+	usart_set_mode(USART2, USART_MODE_TX);
+	usart_set_parity(USART2, USART_PARITY_NONE);
+	usart_set_flow_control(USART2, USART_FLOWCONTROL_NONE);
+	usart_enable(USART2);
 }
 
 
 int main(void)
 {
 	gpio_setup();
+	debuggig_usart_setup();
 	treefrog();
 }
 
@@ -37,6 +61,15 @@ void sleep_for(void)
 
 void play_croak(void)
 {
-	gpio_toggle(PORT_LED, PIN_LED); /* LED on/off */
+	gpio_toggle(PORT_HEARTBEAT_LED, PIN_HEARTBEAT_LED);
+
+	usart_send_blocking(USART2, 'c');
+	usart_send_blocking(USART2, 'r');
+	usart_send_blocking(USART2, 'o');
+	usart_send_blocking(USART2, 'a');
+	usart_send_blocking(USART2, 'k');
+	usart_send_blocking(USART2, '!');
+	usart_send_blocking(USART2, '\r');
+	usart_send_blocking(USART2, '\n');
 }
 
---
+++ target/opencm3/stm32f4_config.h
@@ -0,0 +1,8 @@
+/* board configuration for
+ * STM32F4 Discovery MB1115
+ * (the one with a STM32F411VET6U)
+ */
+
+#define PORT_HEARTBEAT_LED GPIOD
+#define PIN_HEARTBEAT_LED GPIO13
+
