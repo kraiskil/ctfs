a11f2c0eb831a7c8e66903da8842d5d2a621130a
Kalle Raiskila <kraiskil@iki.fi>
2020-03-22 20:15:08 +0200

    Re-open pulse recording on every listen.
    Because pulse_simple_flush() would not flush.
    Which meant that since the frog processes data much
    slower than real time, the input would be terribly old.

--- target/linux/io_pulse.cc
+++ target/linux/io_pulse.cc
@@ -2,6 +2,8 @@
 #include "datatype.h"
 #include "sin_table.h"
 #include "treefrog.h"
+#include <algorithm>
+#include <iomanip>
 #include <iostream>
 
 extern "C" {
@@ -9,14 +11,30 @@ extern "C" {
 }
 
 
-static pa_simple *s = NULL;
 
 void io_init(void)
+{}
+
+void listen_for_croaks(listen_buf_t &buffer)
 {
+	int       rv;
+	int       err;
+	ssize_t   bytes = buffer.size() * sizeof(int16_t);
+	pa_simple *s = NULL;
+
+	/* This doesn't work, so just open the device every time - or the
+	 * data in the buffer would be old
+	rv = pa_simple_flush(s, &err);
+	if (rv) {
+	        std::cerr << "pa_simple_flush() failed with " << err << std::endl;
+	        exit(1);
+	}
+	*/
+
 	static const pa_sample_spec ss = {
 		.format = PA_SAMPLE_S16LE,
 		.rate = config_fs,
-		.channels = 2
+		.channels = 1
 	};
 	int                         error;
 	s = pa_simple_new(
@@ -33,16 +51,8 @@ void io_init(void)
 		std::cerr << "error opening capture device" << std::endl;
 		exit(1);
 	}
-}
-
-void listen_for_croaks(listen_buf_t &buffer)
-{
-	int     rv;
-	int     err;
-	ssize_t bytes = buffer.size() * sizeof(int16_t);
 
 	rv = pa_simple_read(s, buffer.data(), bytes, &err);
-
 	if (rv) {
 		std::cerr << "pa_simple_read() failed with " << err << std::endl;
 		exit(1);
@@ -52,15 +62,30 @@ void listen_for_croaks(listen_buf_t &buffer)
 	 * the FFT doesn't like values much over ~200, or the
 	 * calculations saturate. This scales down the input.
 	 */
+	float max = 0;
 	for (int i = 0; i < buffer.size(); i++) {
-		buffer[i] >>= 6;
+		//buffer[i] >>= 1;
+		//	std::cout << std::setw(6) << buffer[i] << " ";
+		//	if( i%32 == 31)
+		//		std::cout << std::endl;
+		if (buffer[i] > max)
+			max = buffer[i];
 	}
+	max = *std::max_element(buffer.begin(), buffer.end());
+
+	if (max > 256) {
+		std::cout << "Input amplitude at " << max << ", please lower it" << std::endl;
+	}
+
+	pa_simple_free(s);
 }
 
 void play_croak(void)
 {
 	int     croak_samples = config_fs * 1.5 /*seconds*/;
 	int16_t croak_sound[croak_samples];
+
+	std::cout << "croaking" << std::endl;
 
 	static const pa_sample_spec ss = {
 		.format = PA_SAMPLE_S16LE,
@@ -85,5 +110,6 @@ void play_croak(void)
 	}
 
 	pa_simple_write(pas, croak_sound, sizeof(croak_sound), NULL);
+	////pa_simple_free(s);
 }
 
