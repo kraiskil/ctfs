74827eed657d73615c7667a5a919f551d493c21b
Kalle Raiskila <kraiskil@iki.fi>
2020-04-25 14:37:46 +0300

    Add a few audio->numpeaks tests.

--- tests/frog_tones_freq.cc
+++ tests/frog_tones_freq.cc
@@ -9,6 +9,7 @@ public:
 	{
 		FrogTonesTest::SetUp();
 		fs = 48000;
+		srand(42);
 	}
 	void add_audio_sine(int peak, int f_input, float phase = 0.2)
 	{
@@ -16,16 +17,53 @@ public:
 			audio_buffer[i] += peak * sin((2 * M_PI * i * f_input / fs) + phase);
 		}
 	}
+	void add_audio_noise(uint16_t min_ampl, uint16_t max_ampl)
+	{
+		int range = max_ampl - min_ampl + 1; // limits are inclusive
+		for (auto &s : audio_buffer) {
+			uint16_t noise = rand() % range + min_ampl;
+			s += noise;
+		}
+	}
 
 	int fs;
 
 };
 
-TEST_F(FrogTonesFreqTest, fft)
+TEST_F(FrogTonesFreqTest, SingleSine)
 {
 	add_audio_sine(200, 1000);
 	ft->fft();
 	ft->find_peaks();
 	EXPECT_EQ(ft->get_num_peaks(), 1);
+}
+
+TEST_F(FrogTonesFreqTest, TwoSines)
+{
+	add_audio_sine(200, 1000);
+	add_audio_sine(200, 1500);
+	ft->fft();
+	ft->find_peaks();
+	EXPECT_EQ(ft->get_num_peaks(), 2);
+}
+
+TEST_F(FrogTonesFreqTest, SingleSineWithNoise)
+{
+	add_audio_sine(200, 1000);
+	add_audio_noise(30, 40);
+	ft->fft();
+	ft->find_peaks();
+	EXPECT_EQ(ft->get_num_peaks(), 1);
+}
+
+TEST_F(FrogTonesFreqTest, ManySineWithNoise)
+{
+	add_audio_sine(200, 1000);
+	add_audio_sine(100, 1300);
+	add_audio_sine(150, 2800);
+	add_audio_noise(30, 40);
+	ft->fft();
+	ft->find_peaks();
+	EXPECT_EQ(ft->get_num_peaks(), 3);
 }
 
