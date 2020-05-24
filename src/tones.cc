8a09bcbca730b9536ae7faba32ada0c92b8dd22e
Kalle Raiskila <kraiskil@iki.fi>
2020-05-24 17:12:58 +0300

    Add tone_detect debugging tool.

--- src/tones.cc
+++ src/tones.cc
@@ -3,11 +3,9 @@ constexpr tone_frequencies tone_freqs;
 
 enum tones find_tone(uint16_t frequency)
 {
+	/* TODO: how about a smarter search algorithm? */
 	for (int i = A3; i < LAST_TONE; i++) {
 		enum tones e = (enum tones)i;
-		// TODO: allow error margin: current STM32 setup
-		// has a 3% error in frequency (due to inaccurate clocks)
-		// also consider the FFT bin width at low frequencies.
 		if (tone_freqs.get_freq(e) == frequency)
 			return e;
 	}
---
+++ target/opencm3/tone_detect.cc
@@ -0,0 +1,54 @@
+#include "board_config.h"
+#include "datatype.h"
+#include "frog_tones.h"
+#include <cstdio>
+
+void listen_for_croaks(listen_buf_t &buffer);
+
+extern "C" {
+int _write(int file, char *buf, int len)
+{
+	int i;
+	for (i = 0; i < len; i++)
+		usart_send_blocking(USART2, buf[i]);
+	return i;
+}
+}
+
+int main(void)
+{
+	board_setup_clock();
+	board_setup_gpio();
+	board_setup_usart();
+	board_setup_i2s_in();
+	listen_buf_t    abuf;
+	frequency_buf_t fbuf;
+	frog_tones      ft(abuf, fbuf);
+	fft<float>      the_fft;
+
+	the_fft.fs = config_fs_input;
+	the_fft.fft_size = abuf.size();
+
+	while (1) {
+		listen_for_croaks(abuf);
+		the_fft.dc_blocker(abuf);
+		the_fft.run(abuf, fbuf);
+		the_fft.noise_filter(fbuf, 100); // 100Hz seems to be a good choice for Spanish towns.
+		ft.find_peaks();
+
+		printf("Peaks:\n");
+		for (unsigned i = 0; i < ft.get_num_peaks(); i++) {
+			if (ft.as_Hz(ft.get_peak_by_val(i).bin) > 5000)
+				continue;
+			printf("%d Hz (bin %d) - ampl: %d\n",
+			    ft.as_Hz(ft.get_peak_by_val(i).bin),
+			    ft.get_peak_by_val(i).bin,
+			    ft.get_peak_by_val(i).val);
+		}
+		printf("================\n\n");
+
+	}
+
+	return 0;
+}
+
